fd1b455b41e7e8d96023026e527ebf6015990910
Techno-coder <8334328+Techno-coder@users.noreply.github.com>
2019-06-16 15:41:58 +1200

    Add clickable shortcuts to help menu

--- src/shortcut.ts
+++ src/shortcut.ts
@@ -1,9 +1,11 @@
 import hotkeys from "hotkeys-js";
 
 export function register_shortcuts() {
-    register_modal("shift+/", "help_modal");
-    register_modal("a", "authenticate_modal");
-    register_modal("o", "open_problem_modal", () => {
+    document.getElementById("help_activation").onclick = () => toggle_modal("help_modal");
+
+    register_modal("shift+/", "help");
+    register_modal("a", "authenticate");
+    register_modal("o", "open_problem", () => {
         let field = document.getElementById("problem_link") as HTMLInputElement;
         setTimeout(() => {
             field.setSelectionRange(0, field.value.length);
@@ -13,17 +15,9 @@ export function register_shortcuts() {
 }
 
 export function register_modal(shortcut: string, modal_identifier: string, callback?: () => void) {
-    hotkeys(shortcut, () => {
-        let modal = document.getElementById(modal_identifier);
-        let modal_is_hidden = modal.hidden;
-
-        hide_all_modals();
-        modal.hidden = !modal_is_hidden;
-
-        if (callback != undefined) {
-            callback();
-        }
-    });
+    let activation = () => toggle_modal(modal_identifier + "_modal", callback);
+    document.getElementById("help_" + modal_identifier).onclick = activation;
+    hotkeys(shortcut, activation)
 }
 
 export function hide_all_modals() {
@@ -32,5 +26,17 @@ export function hide_all_modals() {
         if (modal instanceof HTMLElement) {
             modal.hidden = true;
         }
+    }
+}
+
+export function toggle_modal(modal_identifier: string, callback?: () => void) {
+    let modal = document.getElementById(modal_identifier);
+    let modal_is_hidden = modal.hidden;
+
+    hide_all_modals();
+    modal.hidden = !modal_is_hidden;
+
+    if (callback != undefined) {
+        callback();
     }
 }
